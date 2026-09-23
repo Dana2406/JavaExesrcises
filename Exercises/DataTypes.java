32c1044a3b835f29cd8e1c8d6b178bf8c236bf75
Dana2406 <danavictoriamorgadof@comunidad.unam.mx>
2026-09-23 20:48:31 +0000

    Corrijo DataTypes: atributos public y doble constructor

--- Exercises/DataTypes.java
+++ Exercises/DataTypes.java
@@ -4,7 +4,19 @@ public class DataTypes {
     public String cadena;
     public int entero;
     public float flotante;
-    char caracter;
-    boolean boleano;
+    public char caracter;
+    public boolean boleano;
 
+    public DataTypes(String cadena, int entero, float flotante, char caracter, boolean boleano) {
+        this.cadena = cadena;
+        this.entero = entero;
+        this.flotante = flotante;
+        this.caracter = caracter;
+        this.boleano = boleano;
+    }
+
+    public DataTypes(){
+        this("Tipos de datos", 20, 3.30f, 'V', true);
+    }
 }
+        
